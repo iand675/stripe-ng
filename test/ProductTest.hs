b86b102b0418512d9f5b94e0bcf6214cbab2e51b
Steven Shaw <steven@steshaw.org>
2020-03-12 18:51:05 +1000

    🐛 Allow product.deactivate_on to be empty
    Empty leading to an empty list of application ids.

--- test/ProductTest.hs
+++ test/ProductTest.hs
@@ -1,14 +1,37 @@
 module ProductTest where
-import Control.Lens hiding (simple)
-import qualified Data.HashMap.Strict as H
-import qualified Stripe.Lens as S
+
+import Test.Tasty.Hspec
+
 import Stripe.Billing.Products
 import Stripe.Utils
-import Test.Tasty.Hspec
+import Stripe.Balance
+import qualified Stripe.Core as Stripe
+import qualified Stripe.Customers as Stripe
+import qualified Stripe.Errors as Stripe
+import qualified Stripe.Utils as Stripe (enumerate, baseQuery)
+
+import Conduit
+import Control.Lens hiding (simple)
+import Control.Monad.Trans (lift)
+import Data.Aeson
+import qualified Conduit as C
+import qualified Data.Conduit.List as C
+import qualified Data.HashMap.Strict as H
+import qualified Data.Text as T
 import qualified Data.Vector as V
+import qualified Stripe.Lens as S
 
 testProduct :: NewProduct
-testProduct = NewProduct Nothing "test product" Service Nothing [] Nothing mempty Nothing Nothing
+testProduct = NewProduct
+  Nothing
+  "test product"
+  Service
+  Nothing
+  []
+  Nothing
+  mempty
+  Nothing
+  Nothing
 
 spec_create_product :: Spec
 spec_create_product = do
@@ -25,3 +48,22 @@ spec_create_product = do
       pure (p, p')
     productMetadata retrievedProduct `shouldBe` H.fromList [("bip", "zoop")]
 -}
+
+spec_list_all_products :: Spec
+spec_list_all_products = do
+  specify "baseQuery" $ do
+    x <- stripeWithEnv $ C.runConduit
+      (Stripe.enumerate (listAllProducts Stripe.baseQuery) .| C.consume)
+    print x
+
+  specify "active services" $ do
+    x <- stripeWithEnv $ C.runConduit
+      (Stripe.enumerate
+          (listAllProducts Stripe.baseQuery
+            { listAllProductsQueryActive = Just True
+            , listAllProductsQueryType_ = Just Service
+            }
+          )
+      .| C.consume
+      )
+    print x
